90661eff6d381c3bc8002c5e0dcf53af1c75b78e
AlexaZ06 <122847494+AlexaZ06@users.noreply.github.com>
2023-10-18 23:55:06 -0600

    add music

--- 04-bubbles/sketch.js
+++ 04-bubbles/sketch.js
@@ -7,20 +7,29 @@
 // - https://p5js.org/examples/3d-geometries.html
 
 // song list
+// the songs were downloaded from YouTube
 // - Merry-go-round of Life
 // - Always With Me
 // - The Path of the wind
 
-// create array
+// create array and variables
 let bubbleArray = [];
-let z = 10;
+let merrygoround;
+let alwayswithme;
+let pathofthewind;
 
+function preload(){
+  merrygoround = loadSound("music-merry-go-round-of-life.mp3");
+  alwayswithme = loadSound("music-always-with-me.mp3");
+  pathofthewind = loadSound("music-path-of-the-wind.mp3");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   makeBubble();
-  debugMode();
-  camera(width, 0, height*1.5, 0, 0, 0, 0, 1, 0);
+  camera(width, height/2, height);
+  currenttime = millis();
+  timelimit = 600000
 }
 
 function draw() {
@@ -35,12 +44,44 @@ function keyTyped() {
   }
 }
 
+
+// play sound
+function mousePressed(){
+  for (let i = 0; i <= bubbleArray.length; i++){
+    let theBubble = bubbleArray[i];
+
+    // song that plays based on spheres predominant colour
+    if (!alwayswithme.isPlaying()
+    && theBubble.r >= theBubble.g
+    && theBubble.r >= theBubble.b ){
+      merrygoround.stop();
+      pathofthewind.stop();
+      alwayswithme.play();
+    }
+    else if (!pathofthewind.isPlaying()
+    && theBubble.g >= theBubble.r
+    && theBubble.g >= theBubble.b){
+    alwayswithme.stop();
+    merrygoround.stop();
+    pathofthewind.play();
+    }
+    else if (!merrygoround.isPlaying() 
+    && theBubble.b >= theBubble.g 
+    && theBubble.b >= theBubble.r){
+      alwayswithme.stop();
+      pathofthewind.stop();
+      merrygoround.play();
+    }
+  }
+  // reset value of i
+}
+  
 function bubbleMovement(){
   for (let theBubble of bubbleArray){
     // move
     theBubble.x = noise(theBubble.time)*width - width/2;
     theBubble.y = noise(theBubble.time - 300)*height - height/2;
-    theBubble.time += 0.001;
+    theBubble.time += 0.0001;
   }
 }
 
@@ -50,7 +91,7 @@ function displayBubble() {
 
   for (let theBubble of bubbleArray){
     // the bubble
-    fill(theBubble.colour);
+    fill(theBubble.r, theBubble.g, theBubble.b);
     translate(theBubble.x, theBubble.y, theBubble.z);
     rotateZ(frameCount* 0.001);
     rotateX(frameCount* 0.001);
@@ -67,7 +108,9 @@ function makeBubble() {
     z: random(-10, 10),
     radius: random(25,75),
     time: random(1000),
-    colour: color(random(255), random(255), random(255), random(255)),
+    r: random(255),
+    g: random(255),
+    b: random(255),
   };
   bubbleArray.push(bubble);
 }
