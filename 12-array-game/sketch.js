1092428742c6b475c94c672b227539096bbe5679
AlexaZ06 <alexwbaby@hotmail.com>
2023-11-06 11:17:50 -0600

    fix format

--- 12-array-game/sketch.js
+++ 12-array-game/sketch.js
@@ -102,43 +102,43 @@ function getCatColors(cols, rows) {
   return colours;
 }
 
-  //assign numbers to colours
-  function number(cols, rows) {   
-    let blocknum = []
-    for (let y = 0; y < rows; y++) {
-      blocknum.push([]);
-      for (let x = 0; x < cols; x++) {
+//assign numbers to colours
+function number(cols, rows) {   
+  let blocknum = [];
+  for (let y = 0; y < rows; y++) {
+    blocknum.push([]);
+    for (let x = 0; x < cols; x++) {
 
-        // white
-        if(catcolour[y][x][0] <= 20 && catcolour[y][x][1] <= 20 && catcolour[y][x][2] <= 20){
-          blocknum[y].push(0);
-        }
-
-        // light grey
-        else if(catcolour[y][x][0] < 200 && catcolour[y][x][0] >= 100 &&
-                catcolour[y][x][1] < 200 && catcolour[y][x][1] >= 100 &&
-                catcolour[y][x][2] < 200 && catcolour[y][x][2] >= 100){
-          blocknum[y].push(1);
-        }
-
-        // dark grey
-        else if(catcolour[y][x][0] < 100 && catcolour[y][x][0] > 20 &&
-          catcolour[y][x][1] < 100 && catcolour[y][x][1] > 20 &&
-          catcolour[y][x][2] < 100 && catcolour[y][x][2] > 20){
-           blocknum[y].push(2);
-        }
-
-        // pink 
-        else if(catcolour[y][x][0] > catcolour[y][x][1] && catcolour[y][x][0] > catcolour[y][x][2]){
-          blocknum[y].push(3);
-        }
-
-        // black
-        if(catcolour[y][x][0] >= 200 && catcolour[y][x][1] >= 200 && catcolour[y][x][2] >= 200){
-          blocknum[y].push(4);
-        }
-        
+      // white
+      if(catcolour[y][x][0] <= 20 && catcolour[y][x][1] <= 20 && catcolour[y][x][2] <= 20){
+        blocknum[y].push(0);
       }
+
+      // light grey
+      else if(catcolour[y][x][0] < 200 && catcolour[y][x][0] >= 100 &&
+              catcolour[y][x][1] < 200 && catcolour[y][x][1] >= 100 &&
+              catcolour[y][x][2] < 200 && catcolour[y][x][2] >= 100){
+        blocknum[y].push(1);
+      }
+
+      // dark grey
+      else if(catcolour[y][x][0] < 100 && catcolour[y][x][0] > 20 &&
+        catcolour[y][x][1] < 100 && catcolour[y][x][1] > 20 &&
+        catcolour[y][x][2] < 100 && catcolour[y][x][2] > 20){
+        blocknum[y].push(2);
+      }
+
+      // pink 
+      else if(catcolour[y][x][0] > catcolour[y][x][1] && catcolour[y][x][0] > catcolour[y][x][2]){
+        blocknum[y].push(3);
+      }
+
+      // black
+      if(catcolour[y][x][0] >= 200 && catcolour[y][x][1] >= 200 && catcolour[y][x][2] >= 200){
+        blocknum[y].push(4);
+      }
+        
     }
-    return blocknum;
   }
+  return blocknum;
+}
